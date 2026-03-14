bf8593118d395c4c525ce2fd36e1eb87258ee8cd
Yesica Gallucci <121723257+yesicagallucci@users.noreply.github.com>
2026-03-14 20:12:55 -0300

    directory js file done

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -5,7 +5,44 @@ const members = document.querySelector('#cards');
 async function getMembers(){
     const response = await fetch(dataURL);
     const data = await response.json();
-    console.log(data);
+    //console.log(data);
 
-    displayMembers(data.companies);
+    displayMembers(data.businesses);
 }
+
+const displayMembers = (businesses) => {
+    businesses.forEach(business => {
+        //create HTML elements
+        const card = document.createElement("section");
+        const name = document.createElement("h3");
+        const logo = document.createElement("img");
+        const address = document.createElement("p");
+        const phone = document.createElement("p");
+        const url = document.createElement("a");
+        const membership = document.createElement("p");       
+        
+        //Connect the content of the Json file to each html element above
+        card.setAttribute("class", "card")
+        logo.setAttribute("src", business.image);
+        logo.setAttribute("alt", `logo of ${business.name}`);
+        logo.setAttribute("loading", "lazy");
+        name.textContent = `${business.name}`;
+        address.textContent = `${business.address}`;
+        phone.textContent = `${business.phone}`;
+        url.setAttribute("href", business.url);
+        url.setAttribute('target', '_blank');
+        membership.textContent =  `Membership level: ${business.membership_level}`
+
+        //appened each parent with its child element/s
+        card.appendChild(name);
+        card.appendChild(logo);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(url);
+        card.appendChild(membership);
+        members.appendChild(card);
+        
+    });
+}
+
+getMembers();
